f096514d91a65333a041046a66b96996a3812603
ASUS <Yehor Kholodiuk>
2022-02-18 17:37:04 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function App() {
+    const [tasks,setTasks] = useState([])
+    const [isLoading, setIsLoading]=useState([])
+  //const [count, setCount] = useState(5)
+  //const [count1, setCount1] = useState(30)
+
+    const loadTasks = () =>{
+        setIsLoading(true)
+        axios({
+         method:'GET',
+            url: 'https://pasv-kanban.herokuapp.com/card'
+        }).then(({data}) =>{
+            setTasks(data)
+        }).finally(()=>{
+            setIsLoading(false)
+        })
+
+        // eslint-disable-next-line react-hooks/rules-of-hooks
+
+
+    }
+    useEffect(() => {console.log('Once')
+        loadTasks() }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    //<div className="App">
+   //Hello {count} {count1}
+      //<button onClick={() => setCount(count + 1)}> Plus</button>
+      //<button onClick={() => setCount1(count1 + 10)}>Plus1</button>
+    //</div>
+      <div>
+          <button onClick={loadTasks}>Load Tasks </button>
+          {isLoading?'...loading':tasks.map(el => <li key={el.id}>{el.name}-{el.status} </li>)}
+
+
+      </div>
   );
 }
 
